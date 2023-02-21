af0fe75ebfa1519b5e76d1656bd596afcaa4dd3c
jake <jjk848798@gmail.com>
2023-02-22 01:13:59 +0530

    feat:speakers

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,15 @@ import Nav1 from '../components/UI/Nav1';
 import About from '../components/About';
 import Sponsors from '../components/UI/Sponsors';
 import Footer from '../components/UI/Footer';
-import Header from '../components/UI/Header'
-
+import Header from '../components/UI/Header';
+import Speakers from '../components/Speakers';
 const Home = () => {
 	return (
 		<div>
 			<Nav1></Nav1>
 			<Header></Header>
 			<About></About>
+			<Speakers></Speakers>
 			{/* <Sponsors></Sponsors> */}
 			<Footer></Footer>
 		</div>
